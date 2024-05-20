8fa5abdceacb8380ba4fddbad23d046beb455da7
tarik59 <74472702+tarik59@users.noreply.github.com>
2024-05-20 19:06:38 +0200

    refactor

--- BookingSystem.Interfaces/Contracts/IAccountService.cs
+++ BookingSystem.Interfaces/Contracts/IAccountService.cs
@@ -1,9 +1,4 @@
 ﻿using BookingSystem.Models.User;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookingSystem.Interfaces.Contracts
 {
